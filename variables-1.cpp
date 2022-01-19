ec3a9d8eece051956a50a0e4ec31459bb2758ca4
Your Name <you@example.com>
2022-01-19 23:42:35 +0500

    Variables 01

---
+++ variables-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int slices; //declaration of a variable
+    slices = 5; //initialization of a variable
+
+    cout << "You have " << slices << " slices of pizza." << endl;
+}
