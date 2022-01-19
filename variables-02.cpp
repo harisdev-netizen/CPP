2e35aa8b68377d0a1af2db3b63e9f596024ea2a8
Your Name <you@example.com>
2022-01-19 23:53:31 +0500

    Variables second lesson code

---
+++ variables-02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int slices; //variable declared
+    cout << "YO! Fatty how many pieces of slices of pizza you eat?: ";
+    cin >> slices; //cin is used for taking data from the user
+    cout << "You have " << slices << " slices of pizza." << endl;
+}
